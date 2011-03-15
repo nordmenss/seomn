ab36128a048193aeaf00cb7c8305319344f00c15
nordmenss <nordmenss@gmail.com>
2011-03-16 01:04:35 +0200

    d

--- seomn/__init__.py
+++ seomn/__init__.py
@@ -1,6 +1,7 @@
 VERSION = '0.0'
-
+from django.conf import settings
 from urlparse import urlparse
+from django.db import connections
 
 def get_host(href):
     parse_object = urlparse(href)
@@ -11,3 +12,27 @@ def execute(db,sql):
     cursor = connections[db].cursor()
     cursor.execute(sql)
     transaction.commit_unless_managed(using=db)
+
+def loadrow(db,sql):
+    from django.db import connections, transaction
+    cursor = connections[db].cursor()
+    cursor.execute(sql)
+    return cursor.fetchone()
+
+def loadrows(db,sql):
+    from django.db import connections, transaction
+    cursor = connections[db].cursor()
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+def fqdn_redirect(fqdn):
+    return settings.FORCE_SCRIPT_NAME+fqdn
+
+def connect2db(fqdn):#make connection
+    params=loadrow("main_db","SELECT _connection_info.get('"+fqdn+"')")
+    ip=params.ip
+    dbname=params.dbname
+    user=params.username
+    password=params.password
+    port=params.port
+    settings.DATABASES['cluster']={'ENGINE':'postgresql_psycopg2','NAME':dbname,'USER':user, 'PASSWORD':password,'HOST':ip,'PORT':port}
