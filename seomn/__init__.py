3a0e623b83fc3bd71ead78015ce39686bc1f7288
nordmenss <nordmenss@gmail.com>
2011-05-05 13:13:27 +0300

    f

--- seomn/__init__.py
+++ seomn/__init__.py
@@ -50,6 +50,12 @@ def load_str(db,sql):
         return str(row[0])
     return None
 
+def load(db,sql):
+    row=load_row(db,sql)
+    if row!=None:
+        return row[0]
+    return None
+
 def load_date(db,sql):
     row=load_row(db,sql)
     if row!=None:
